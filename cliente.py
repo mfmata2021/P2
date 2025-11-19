94bcb555841763d826172e0fd0aa2894e097456c
mf.mata.2021 <mf.mata.2021@alumnos.urjc.es>
2025-11-19 16:47:49 +0100

    agregado el lock en el servidor

--- cliente.py
+++ cliente.py
@@ -1,15 +1,34 @@
 import socket
+import pickle
 
 # IMPORTANTE RECORDAR!!!! El puerto y la ip se pide por linea de comandos, recordar cambiar !!!
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Creamos el socket 
 cliente.connect((socket.gethostname(), 5555)) #Lo conectamos a la dirección IP y al puerto 
 
 
-# -- INICIO Y CONEXIÓN
 """El cliente pide el nombre de usuario y se conecta al servidor, enviando su 
 credencial"""
 while True:
+
+    # Lista de usuarios que tiene el servidor conectado, así podemos comprobar que no se repita el mismo nombre
+    data = cliente.recv(1024)
+    usuarios_bloqueados = pickle.loads(data) #Aquí está la lista de usuarios bloqueados que tiene el servidor 
+
     print("\n-- Bienvenido a la biblioteca de música --\n")
-    print("Antes de iniciar, introduzca un nombre de usuario: ")
-    usuario = input("> ")
-    cliente.sendall(usuario.encode())
+    print("Antes de iniciar, introduzca un nombre de usuario")
+
+    # -- INICIO Y CONEXIÓN
+    # Bucle para comprobar que no se repita el mismo nombre de usuario
+    while True: 
+        usuario = input("> ")
+        if usuario not in usuarios_bloqueados:
+            cliente.sendall(usuario.encode())
+            break
+        else:
+            print("El nombre de usuario no está disponible. Inténtelo de nuevo")
+
+    # -- SINCRONIZACIÓN INICIAL Y DESCARGA
+    
+    # -- TRABAJO LOCAL
+
+    # -- CIERRE Y SINCRONIZACIÓN FINAL
--- servidor.py
+++ servidor.py
@@ -1,46 +1,60 @@
 import socket
 import threading
+import pickle
+import json
+from .datos_server import *
 
 
-#Biblioteca que contendrá a los {"usuario" : usuario_socket} que se conecten al servidor y que no pueden ser usuados por nuevos clientes s
+# Biblioteca que contendrá a los {"usuario" : usuario_socket} que se conecten al servidor y que no pueden ser usuados por nuevos clientes s
 usuarios_activos = []
-#Para poder modificar variables globales y evitar condiciones de carrera, usamos el lock 
+# Para poder modificar variables globales y evitar condiciones de carrera, usamos el lock
 lock = threading.Lock()
 
-#IMPORTANTE RECORDAR!!!! EL puerto se pide por linea de comandos, recordar cambiar !!!
-print("Arrancando servidor")
+# IMPORTANTE RECORDAR!!!! EL puerto se pide por linea de comandos, recordar cambiar !!!
+print("\nArrancando servidor...")
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind((socket.gethostname(), 5555))
 servidor.listen()
 
-#Esta función es para comprobar si el usuario que ha escogido el cliente está disponible 
-def comprobar_disponibilidad(u):
-    if u not in usuarios_activos:
-        usuarios_activos.append(u)
-        return True
-    return False 
-
-
-
 
 def comunicacion (cliente, addr):
+
+    global usuarios_activos
+
     if cliente:
         print(f"Cliente conectado en: {addr}")
 
         while True:
-            usuario = cliente.recv(1024) #Aquí se recibiría el nombre de usuario de los clientes que se conectan
+            # -- Envío al cliente la lista de usuarios activos en el servidor
+
+            # IMPORTANTE:Si un hilo puede modificar la lista, entonces todas las lecturas deben protegerse también
+            try: 
+                lock.acquire()
+                data = pickle.dumps(usuarios_activos)
+            finally:
+                lock.release()
+            cliente.sendall(data)
+
+            # -- Aquí se recibiría el nombre de usuario de los clientes que se conectan
+            usuario = cliente.recv(1024) 
             if not usuario:
-                print("Cerrando conexión")
+                print(f"Cliente desconectado: {addr}")
                 cliente.close()
                 break 
             u = usuario.decode() #Aquí extraemos el nombre de usuario que ha elegido el cliente
-            
+            try:
+                lock.acquire()
+                usuarios_activos.append(u)
+            finally:
+                lock.release()
+
+        
+
+            # -- Control de usuarios activos
+            print(usuarios_activos)
 
 
-
-
-
-#Esperamos a clientes infinitamente a menos que se interrumpa por teclado 
+# Esperamos a clientes infinitamente a menos que se interrumpa por teclado
 try:
     while True: 
         # Espera a que llegue un nuevo cliente
